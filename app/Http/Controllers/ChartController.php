3ea483ed1ea665f7656c1d4cfd32c9dab1254db5
Indra <ikaliana@yahoo.com>
2020-10-13 08:55:30 +0700

    Create ChartController.php
    Controller for Chart process

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChartController extends Controller
+{
+    public function create() 
+    {
+    	//
+    }
+
+    public function upload($code)
+    {
+    	//
+    }
+}
