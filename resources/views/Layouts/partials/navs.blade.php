24a83c391035b1681c71f5c16669bedf34393c81
Indra <ikaliana@yahoo.com>
2020-10-22 10:39:44 +0700

    Update navs.blade.php
    - add home url in the logo
    - hide the menu only in home page

--- resources/views/Layouts/partials/navs.blade.php
+++ resources/views/Layouts/partials/navs.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-info">
   <div class="container-xl">
     @if(!Request::is('/'))
-    <a class="navbar-brand px-4 border rounded" href="#">Visualisasi Data</a>
+    <a class="navbar-brand px-4 border rounded" href="{{ route('home') }}">Visualisasi Data</a>
     @endif
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-links" aria-controls="nav-links" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,8 +9,9 @@
 
     <div class="collapse navbar-collapse" id="nav-links">
       <ul class="navbar-nav mr-auto ml-auto">
+      @if(!Request::is('/'))
         <li class="nav-item active">
-          <a class="nav-link" href="#">Chart</a>
+          <a class="nav-link" href="{{ route('chart.create') }}">Chart</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="#">Map</a>
@@ -18,8 +19,9 @@
         <li class="nav-item active">
           <a class="nav-link" href="#">Table</a>
         </li>
+      @endif
       </ul>
-        <a class="btn btn-light" href="#">Login</a>
+      <a class="btn btn-light" href="#">Login</a>
     </div>
   </div>
 </nav>
