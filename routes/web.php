00659c39da4e52ab4273208493a6418521c35239
Indra <ikaliana@yahoo.com>
2020-11-05 23:16:19 +0700

    Update web.php
    Add chart group route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('chart')->group(function () {
+	Route::get('create', 'ChartController@create')->name('chart.create');
+	Route::get('upload/{code}', 'ChartController@upload')->name('chart.upload.get');
+	Route::post('upload/{code}', 'ChartController@upload')->name('chart.upload.post');
+	Route::get('check/{code}', 'ChartController@check')->name('chart.check.get');
+	Route::post('check/{code}', 'ChartController@check')->name('chart.check.post');
+	Route::get('setting/{code}', 'ChartController@setting')->name('chart.setting.get');
+	Route::post('setting/{code}', 'ChartController@setting_post')->name('chart.setting.post');
+	Route::get('publish/{code}', 'ChartController@publish')->name('chart.publish');
+});
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
