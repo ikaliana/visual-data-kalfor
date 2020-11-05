d0141575449f62798f073324a15d02317109b830
Indra <ikaliana@yahoo.com>
2020-11-05 23:12:15 +0700

    Update ChartController.php
    Update everythings

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -2,17 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ChartService;
 use Illuminate\Http\Request;
+// use App\Imports\DataImport;
+// use Illuminate\Support\Str;
+// use Illuminate\Support\Facades\Storage;
+// use Illuminate\Filesystem\Filesystem;
 
 class ChartController extends Controller
 {
-    public function create() 
+	private $service; 
+
+    public function __construct()
     {
-    	//
+        $this->service = new ChartService();
     }
 
-    public function upload($code)
+    public function create() 
     {
-    	//
+    	return $this->service->NewChart();
+    }
+
+    public function upload(Request $request, $code)
+    {
+    	if ($request->isMethod('get')) return $this->service->ShowUpload($request, $code);
+
+    	return $this->service->UploadData($request, $code);
+    }
+
+    public function check(Request $request, $code)
+    {
+    	if ($request->isMethod('post')) return $this->service->SaveCheckedData($request, $code);
+
+  //   	$data = $this->service->LoadOriginDataArray($request, $code);
+    	$data = $this->service->GetChartData($code);
+
+    	$columns = [];
+		if(count($data) > 0) {
+			$head = array_keys($data[0]);
+			$sample = array_values($data[0]);
+
+			for($i = 0; $i < count($head); $i++) {
+				$tmp = [];
+				$tmp['title'] = $head[$i];
+				$tmp['type'] = (is_numeric($sample[$i])) ? 'numeric' : 'text';
+
+				array_push($columns,$tmp);
+			}
+		}
+
+    	// $data = array_slice($data,1);
+
+    	return view('chart.check', compact('code','columns','data'));
+    }
+
+    public function setting(Request $request, $code)
+    {
+    	// $data = $this->service->LoadOriginDataJson($request, $code);
+		$data = $this->service->GetChartData($code);
+
+    	$columns = [];
+    	foreach($data[0] as $key => $val) {
+    		array_push($columns, $key);
+    	}
+
+		return view('chart.setting', compact('code','columns','data'));
+    }
+
+    public function setting_post(Request $request, $code)
+    {
+    	return $this->service->SaveChartOptions($request, $code);
+    }
+
+    public function publish(Request $request, $code)
+    {
+		$chart_settings = $this->service->GetChartOptions($code);
+		$data = $this->service->GetChartData($code);
+
+		// return file_get_contents($setting_file);
+		return view('chart.publish', compact('code','chart_settings','data'));
     }
 }
