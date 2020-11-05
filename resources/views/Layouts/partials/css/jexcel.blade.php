d6f1a0848c61ba61ad5745fe1a751cb8799c5539
Indra <ikaliana@yahoo.com>
2020-11-05 23:14:45 +0700

    Create jexcel.blade.php
    partial file to include jexcel  & jsuites library css

---
+++ resources/views/Layouts/partials/css/jexcel.blade.php
@@ -0,0 +1,3 @@
+<link href="{{ asset('css/jexcel.css') }}" rel="stylesheet">
+<link href="{{ asset('css/jsuites.css') }}" rel="stylesheet">
+<!-- <link href="{{ asset('css/jexcel.datatables.css') }}" rel="stylesheet"> -->
