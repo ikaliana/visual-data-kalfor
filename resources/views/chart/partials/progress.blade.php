da42a56e8f8317f044d211112bbb530874e53570
Indra <ikaliana@yahoo.com>
2020-11-12 11:09:20 +0700

    Create progress.blade.php
    header for step of the process
    button prev & next for move to prev/next process

---
+++ resources/views/chart/partials/progress.blade.php
@@ -0,0 +1,28 @@
+<div class="row mb-3 mt-4 pt-3">
+    <div class="col pr-3">
+        <button type="button" class="btn btn-lg btn-info btn-block text-white" id="prev-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah sebelumnya">&laquo;</button>
+    </div>
+    <div class="progress col-sm-3 mr-3 px-0" style="height: 44px; font-size: 1.125rem;">
+        <span class="my-auto mx-auto step-label-1">Pilih sumber data</span>
+        <div class="progress-bar step-progress-1" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            Pilih sumber data
+        </div>
+    </div>
+    <div class="progress col-sm-3 px-0" style="height: 44px; font-size: 1.125rem;">
+        <span class="my-auto mx-auto step-label-2">Validasi data</span>
+        <div class="progress-bar step-progress-2" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            Validasi data
+        </div>
+    </div>
+    <div class="progress col-sm-3 ml-3 px-0" style="height: 44px; font-size: 1.125rem;">
+        <span class="my-auto mx-auto step-label-3">Setting grafik</span>
+        <div class="progress-bar step-progress-3" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            Setting grafik
+        </div>
+    </div>
+    <div class="col">
+        <button type="button" class="btn btn-lg btn-info btn-block text-white" id="next-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah selanjutnya">&raquo;</button>
+    </div>
+</div>
