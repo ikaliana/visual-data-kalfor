b4967c767242da39a1097efa30811ac8dca42de5
Indra <ikaliana@yahoo.com>
2020-11-14 06:51:38 +0700

    Update navs.blade.php
    include icon in Header logo, links, and login button

--- resources/views/Layouts/partials/navs.blade.php
+++ resources/views/Layouts/partials/navs.blade.php
@@ -1,7 +1,10 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-info">
+<nav class="navbar navbar-expand-sm navbar-dark bg-info" style="min-height: 78px;">
   <div class="container-xl">
     @if(!Request::is('/'))
-    <a class="navbar-brand px-4 border rounded" href="{{ route('home') }}">Visualisasi Data</a>
+    <a class="navbar-brand px-4 border rounded" href="{{ route('home') }}">
+      <img src="{{ asset('icons/logo.png') }}" width="40" height="40" class="d-inline-block mr-2 my-1" alt="" loading="lazy">
+      Visualisasi Data
+    </a>
     @endif
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-links" aria-controls="nav-links" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,17 +14,17 @@
       <ul class="navbar-nav mr-auto ml-auto">
       @if(!Request::is('/'))
         <li class="nav-item active">
-          <a class="nav-link" href="{{ route('chart.create') }}">Grafik</a>
+          <a class="nav-link" href="{{ route('chart.create') }}"><i class="far fa-chart-bar mr-1"></i> Grafik</a>
+        </li>
+        <li class="nav-item active mx-3">
+          <a class="nav-link" href="#"><i class="fas fa-map-marked-alt mr-1"></i> Peta</a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link" href="#">Peta</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Tabel</a>
+          <a class="nav-link" href="#"><i class="fas fa-table mr-1"></i> Tabel</a>
         </li>
       @endif
       </ul>
-      <a class="btn btn-light" href="#">Login</a>
+      <a class="btn btn-light" href="#">Login <i class="fas fa-sign-in-alt mr-1"></i></a>
     </div>
   </div>
 </nav>
