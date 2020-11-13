420400d81a02f0f9afdaffa33398711e5d3cc33d
Indra <ikaliana@yahoo.com>
2020-11-14 06:55:12 +0700

    Update progress.blade.php
    add icon to all button

--- resources/views/chart/partials/progress.blade.php
+++ resources/views/chart/partials/progress.blade.php
@@ -1,28 +1,28 @@
 <div class="row mb-3 mt-4 pt-3">
     <div class="col pr-3">
-        <button type="button" class="btn btn-lg btn-info btn-block text-white" id="prev-process" 
-            data-toggle="tooltip" data-placement="top" title="Langkah sebelumnya">&laquo;</button>
+        <button type="button" class="btn btn-lg btn-primary btn-block text-white" id="prev-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah sebelumnya"><i class="fas fa-angle-double-left"></i></button>
     </div>
     <div class="progress col-sm-3 mr-3 px-0" style="height: 44px; font-size: 1.125rem;">
-        <span class="my-auto mx-auto step-label-1">Pilih sumber data</span>
-        <div class="progress-bar step-progress-1" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-            Pilih sumber data
+        <span class="my-auto mx-auto step-label-1"><i class="fas fa-database mr-2"></i> Pilih sumber data</span>
+        <div class="step-progress-1 progress-bar" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            <i class="fas fa-database mr-2"></i> Pilih sumber data
         </div>
     </div>
     <div class="progress col-sm-3 px-0" style="height: 44px; font-size: 1.125rem;">
-        <span class="my-auto mx-auto step-label-2">Validasi data</span>
-        <div class="progress-bar step-progress-2" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-            Validasi data
+        <span class="my-auto mx-auto step-label-2"><i class="fas fa-spell-check mr-2"></i> Validasi data</span>
+        <div class="step-progress-2 progress-bar" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            <i class="fas fa-spell-check mr-2"></i> Validasi data
         </div>
     </div>
     <div class="progress col-sm-3 ml-3 px-0" style="height: 44px; font-size: 1.125rem;">
-        <span class="my-auto mx-auto step-label-3">Setting grafik</span>
-        <div class="progress-bar step-progress-3" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-            Setting grafik
+        <span class="my-auto mx-auto step-label-3"><i class="fas fa-sliders-h mr-2"></i> Setting grafik</span>
+        <div class="step-progress-3 progress-bar" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            <i class="fas fa-sliders-h mr-2"></i> Setting grafik
         </div>
     </div>
     <div class="col">
-        <button type="button" class="btn btn-lg btn-info btn-block text-white" id="next-process" 
-            data-toggle="tooltip" data-placement="top" title="Langkah selanjutnya">&raquo;</button>
+        <button type="button" class="btn btn-lg btn-primary btn-block text-white" id="next-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah selanjutnya"><i class="fas fa-angle-double-right"></i></button>
     </div>
 </div>
