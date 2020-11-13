cdbeabf8a3a47d5526f210e0b93318ef1edc3df6
Indra <ikaliana@yahoo.com>
2020-11-14 06:50:05 +0700

    Update app.js
    include font-awesome

--- public/js/app.js
+++ public/js/app.js
@@ -37282,8 +37282,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Indra\sourcecode\laravel\HomesteadCode\visdata\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Indra\sourcecode\laravel\HomesteadCode\visdata\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/visdata/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/visdata/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
