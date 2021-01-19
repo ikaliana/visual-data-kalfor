61070a564c0cbc9ee9a34e96caa03ed7668e7f02
Indra <ikaliana@yahoo.com>
2021-01-19 22:48:48 +0700

    Create progress.blade.php

---
+++ resources/views/map/partials/progress.blade.php
@@ -0,0 +1,16 @@
+<div class="d-flex justify-content-center mb-4 mt-4 pt-2 pb-2"
+    style="background-color: rgb(108 178 235 / 0.1); border-bottom: 2px solid rgb(108 178 235 / 0.1); border-top: 2px solid rgb(108 178 235 / 0.1);">
+    <div class="mr-auto col-sm-2">
+        <button type="button" class="btn btn-lg btn-primary text-white" id="prev-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah sebelumnya"><i class="fas fa-angle-double-left"></i></button>
+    </div>
+    <div>
+        <h1 class="mb-0 mt-1 map-header">Title</h1>
+    </div>
+    <div class="ml-auto col-sm-2 text-right">
+        <button type="button" class="btn btn-lg btn-primary text-white" id="next-process" 
+            data-toggle="tooltip" data-placement="top" title="Langkah selanjutnya"><i class="fas fa-angle-double-right"></i></button>
+        <button type="button" class="btn btn-lg btn-block btn-primary text-white" id="analisis" 
+            data-toggle="tooltip" data-placement="top" title="Lakukan analisis">Analisis <i class="fas fa-angle-double-right"></i></button>
+    </div>
+</div>
