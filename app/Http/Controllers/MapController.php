aca277c303e5fe5e2e755bb2974d651c97ea0bfa
Indra <ikaliana@yahoo.com>
2021-01-19 22:44:55 +0700

    Create MapController.php

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\MapService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class MapController extends Controller
+{
+	private $service; 
+
+    public function __construct()
+    {
+        $this->service = new MapService();
+    }
+
+    public function create()
+    {
+    	$code = Str::lower(Str::random(8));
+    	return redirect()->route('map.source.list', [$code]);
+    }
+
+    public function source(Request $request, $code)
+    {
+    	if ($request->isMethod('get')) return $this->service->listShapefile1($request, $code);
+
+    	$key = "shapedata.".$code;
+    	session()->put($key, $request->data);
+
+    	return redirect()->route('map.setting.get', [$code]);
+    	//return $request->data;
+    }
+
+    public function upload(Request $request)
+    {
+    	$this->service->shpUploader1($request);
+    }
+
+    public function setting(Request $request, $code)
+    {
+    	if ($request->isMethod('get')) return $this->service->listField1($request,$code);
+
+    	$key = "aggregatdata.".$code;
+    	session()->put($key, $request->data);
+
+    	return redirect()->route('map.publish', [$code]);
+    }
+
+    public function publish(Request $request, $code)
+    {
+    	return $this->service->agregat($request,$code);
+    }
+
+    public function geojson(Request $request)
+    {
+    	return $this->service->geojson($request);
+    }
+
+    public function legend(Request $request)
+    {
+    	return $this->service->getLegend();
+    }
+}
